84382977e763958f64744f2a018df2591073a385
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2025-02-19 12:41:43 -0500

    Implemented jpl to md converter to the same level as the html one

---
+++ jpl-md/example.md
@@ -0,0 +1,19 @@
+# Example page
+
+This is a page to demo JPL.
+Here are some things it can do:
+
+* Headers
+* Paragraphs
+* Lists (like this one)
+* [Lists can have elements, too!](https://turnoffthetv.xyz)
+
+[There are links, too!](https://turnoffthetv.xyz)
+
+This is styled inline
+
+This is styled using a class
+
+This is also styled using a class. Two of them!
+
+This is styled both inline and with a class
--- jpl-md/index.mjs
+++ jpl-md/index.mjs
@@ -6,12 +6,57 @@
 //@ts-check
 
 /**
+ * Convert JPL element to Markdown element
+ * @param {Object} e - JPL JSON element object
+ * @returns {string} - Markdown element output
+ */
+function mdEl(e){
+	let md = "";
+	switch(e.type){
+		case "paragraph":
+			md+=e.content+"\n";
+		break;
+		case "link":
+			if(e.content){
+				md+=`[${e.content}](${e.href})\n`;
+			}else{
+				md+=`<${e.href}>\n`;
+			}
+		break;
+		case "header":
+			for(let i=0;i<e.level;i++){
+				md+="#";
+			}
+
+			md+=` ${e.content}\n`;
+		break;
+		case "list":
+			for(let j=0;j<e.items.length;j++){
+				if(e.items[j].type){
+					md+="* ";
+					md+=mdEl(e.items[j]);
+				}else{
+					md+=`* ${e.items[j].content}\n`;
+				}
+			}
+		break;
+	}
+	return md;
+}
+
+/**
  * Convert JPL to Markdown
  * @param {string} jpl - JPL JSON string
  * @returns {string} - Markdown output
  */
 export function jplToMd(jpl){
+	let jplObject = JSON.parse(jpl);
 	let md = "";
+
+	for(let i=0;i<jplObject.elements.length;i++){
+		md+=mdEl(jplObject.elements[i]);
+		if(i!==jplObject.elements.length-1) md+="\n";
+	}
 
 	return md;
 }
